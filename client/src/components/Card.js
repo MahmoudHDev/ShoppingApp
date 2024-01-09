93392f998884c103794138f34732e0b8461c5db7
Mahmoud Hashim <mahmoud.ios.dev@gmail.com>
2024-01-10 00:13:41 +0200

    adding some styles to the cards

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -5,7 +5,8 @@ export default function Card(props) {
             <h5 className="card-title">{props.title}</h5>
             <p className="card-text">{props.description}</p>
             <p className="card-text"><small className="text-body-secondary">price: {props.price} L.E</small></p>
-            <button type="button" className="btn btn-outline-light">add to cart</button>
+            <button type="button" className="btn btn-outline-light cart-btn">add to cart <i class="bi bi-cart-plus"></i></button>
+            
         </div>
     </div>;
 }
