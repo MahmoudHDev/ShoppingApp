d2ff5aef555c964564c5345ef8d6f06c3f34429a
Mahmoud Hashim <mahmoud.ios.dev@gmail.com>
2024-01-17 17:01:09 +0200

    changing form style, and adding some classess

--- client/src/components/Login/Register.js
+++ client/src/components/Login/Register.js
@@ -34,43 +34,66 @@ export default function Register() {
                 <div className="container col-6">
 
                     <div class="input-group">
-                        <input type="text" class="form-control" placeholder="First name" aria-label="fName" name="fName" onChange={handleChange} value={newUserInfo.fName || ""} />
-                        <span class="input-group-text">  </span>
-                        <input type="text" class="form-control" placeholder="Last name" aria-label="lName" name="lName" onChange={handleChange} value={newUserInfo.lName || ""} />
+                        <div class="col-md-6">
+                            <label for="firstName" class="form-label" >First Name</label>
+                            <input type="text" class="form-control" id="firstName" placeholder="John" aria-label="fName" name="fName" onChange={handleChange} value={newUserInfo.fName || ""} />
+                        </div>
+                        <div class="col-md-6">
+                            <label for="lastName" class="form-label">Last Name</label>
+                            <input type="text" class="form-control" id="lastName" placeholder="Smith" aria-label="lName" name="lName" onChange={handleChange} value={newUserInfo.lName || ""} />
+                        </div>
                     </div>
 
-                    <label>Username:</label>
-                    <input type="text" className="form-control" aria-label="username"
-                        aria-describedby="inputGroup-sizing-default" onChange={handleChange} name="username" value={newUserInfo.username || ""} />
 
-                    <label>Email address:</label>
-                    <input type="email" className="form-control" aria-label="email"
-                        aria-describedby="inputGroup-sizing-default" name="email" onChange={handleChange} value={newUserInfo.email || ""} />
 
-                    <label>Password:</label>
-                    <input type="password" className="form-control" aria-label="password" name="password"
-                        aria-describedby="inputGroup-sizing-default" onChange={handleChange} value={newUserInfo.password || ""} />
-                    <div id="passwordHelpBlock" class="form-text">
-                        Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                    <div className="col">
+                        <label className="form-label">Username:</label>
+                        <input type="text" className="form-control" aria-label="username"
+                            aria-describedby="inputGroup-sizing-default" onChange={handleChange} name="username" value={newUserInfo.username || ""} />
                     </div>
 
-                    <label>Re-Enter Password:</label>
-                    <input type="password" className="form-control" aria-label="passConf" name="passConf"
-                        aria-describedby="inputGroup-sizing-default" onChange={handleChange} value={newUserInfo.passConf || ""} />
+                    <div className="col">
+                        <label className="form-label">Email address:</label>
+                        <input type="email" className="form-control" aria-label="email"
+                            aria-describedby="inputGroup-sizing-default" name="email" onChange={handleChange} value={newUserInfo.email || ""} />
+                    </div>
 
-                    <label>Birthday:</label>
-                    <input type="date" id="birthday" name="birthday" className="form-control" aria-label="birthday"
-                        aria-describedby="inputGroup-sizing-default" onChange={handleChange} min="1980-01-01" max="2005-01-01" value={newUserInfo.birthday || "1980-01-01"} />
+                    <div className="col">
+                        <label className="form-label">Password:</label>
+                        <input type="password" className="form-control" aria-label="password" name="password"
+                            aria-describedby="inputGroup-sizing-default" onChange={handleChange} value={newUserInfo.password || ""} />
+                        <div id="passwordHelpBlock" class="form-text">
+                            Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                        </div>
+                    </div>
 
-                    <select name="gender" id="" className="form-select form-select-md my-3">
-                        <option selected>Select Gender</option>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                    </select>
+                    <div className="col">
+                        <label className="form-label">Re-Enter Password:</label>
+                        <input type="password" className="form-control" aria-label="passConf" name="passConf"
+                            aria-describedby="inputGroup-sizing-default" onChange={handleChange} value={newUserInfo.passConf || ""} />
+                    </div>
+
+
+                    <div className="input-group d-flex form-gr">
+                        <div className="col-xl-5">
+                            <label className="form-label">Date of Birth:</label>
+                            <input type="date" id="birthday" name="birthday" className="form-control" aria-label="birthday"
+                                aria-describedby="inputGroup-sizing-default" onChange={handleChange} min="1980-01-01" max="2005-01-01" value={newUserInfo.birthday || "1980-01-01"} />
+                        </div>
+
+                        <div className="col-xl-5">
+                            <label className="form-label">Gender:</label>
+                            <select name="gender" id="" className="form-select form-select-md">
+                                <option selected>Select gender</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </select>
+                        </div>
+                    </div>
+
 
                     <div className="d-grid">
-                        <button type="submit" className="btn btn-primary mt-3" onSubmit={handleSubmit}> Register</button>
-
+                        <button type="submit" className="btn btn-primary mt-3 reg-btn" onSubmit={handleSubmit}> Register</button>
                     </div>
 
                     <div className="container my-4">
